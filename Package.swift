0a4e2a02a421837f144fab6f004efc39675aed0c
Victor Zhu <ghoyelo@gmail.com>
2024-12-24 20:51:28 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     products: [
         .library(
             name: "SVProgressHUD",
-            type: .static,
             targets: ["SVProgressHUD"]
         )
     ],
